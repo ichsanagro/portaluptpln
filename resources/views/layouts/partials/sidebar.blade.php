ee17276be69509c5479ba0f2b33ec9054eccbf84
Hafidzarr30 <hafidzar.bkl16@gmail.com>
2026-01-19 15:55:05 +0700

    perbaikan sidebar

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -11,7 +11,7 @@
             <svg class="h-8 w-auto text-yellow-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z" />
             </svg>
-            <span class="text-xl font-semibold">UPT PLN</span>
+            <span class="text-xl font-bold">PLN UPT BENGKULU</span>
         </a>
     </div>
 
